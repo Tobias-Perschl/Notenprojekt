4bd1bc3b723f0e10a74a625a662c9c87091218c3
Dom1n1kR <dinikr015@gmail.com>
2018-07-02 11:10:23 +0200

    dischda

--- src/NOTE.java
+++ src/NOTE.java
@@ -14,63 +14,82 @@ public class NOTE{
         switch(punkte){
             case 15:
                 return 0.66;
-
         }
 
-        if(punkte == 15){
-            return 0.66;
-        }
-        if(punkte == 14){
-            return 1.0;
-        }
-        if(punkte == 13){
-            return 1.33;
-        } if(punkte == 12){
-            return 1.66;
-        }
-        if(punkte == 12){
-            return 1.66;
-        }
-        if(punkte == 11){
-            return 2.00;
-        }
-        if(punkte == 10){
-            return 2.33;
-        }
-        if(punkte == 9){
-            return 2.66;
-        }
-        if(punkte == 8){
-            return 3.00;
-        }
-        if(punkte == 7){
-            return 3.33;
-        }
-        if(punkte == 6){
-            return 3.66;
-        }
-        if(punkte == 5){
-            return 4.00;
-        }
-        if(punkte == 4){
-            return 4.33;
-        }
-        if(punkte == 3){
-            return 4.66;
-        }
-        if(punkte == 2){
-            return 5.0;
-        }
-        if(punkte == 1){
-            return 5.33;
-        }
-        if(punkte == 0){
-            return 5.66;
+        switch(punkte){
+            case 14:
+                return 1.00;
         }
 
-        return 0;
+        switch(punkte){
+            case 13:
+                return 1.33;
+        }
 
-    }
+        switch(punkte){
+            case 12:
+                return 1.66;
+        }
+
+        switch(punkte){
+            case 11:
+                return 2.00;
+        }
+
+        switch(punkte){
+            case 10:
+                return 2.33;
+        }
+
+        switch(punkte){
+            case 9:
+                return 2.66;
+        }
+
+        switch(punkte){
+            case 8:
+                return 3.00;
+        }
+
+        switch(punkte){
+            case 7:
+                return 3.33;
+        }
+
+        switch(punkte){
+            case 6:
+                return 3.66;
+        }
+
+        switch(punkte){
+            case 5:
+                return 4.00;
+        }
+
+        switch(punkte){
+            case 4:
+                return 4.33;
+        }
+
+        switch(punkte){
+            case 3:
+                return 4.66;
+        }
+
+        switch(punkte){
+            case 2:
+                return 5.00;
+        }
+
+        switch(punkte){
+            case 1:
+                return 5.33;
+        }
+
+        switch(punkte){
+            case 0:
+                return 6.00;
+        }
     int PunkteGeben(){
         return punkte;
     }
